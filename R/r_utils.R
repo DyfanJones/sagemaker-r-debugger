52ba276d78f77cd4ea30333ba669016cd8b377ba
DyfanJones <dyfan.jones@shopdirect.com>
2021-08-11 10:37:12 +0100

    feature: r helper functions

---
+++ R/r_utils.R
@@ -0,0 +1,15 @@
+`%||%` <- function(x, y) if (is.null(x)) return(y) else return(x)
+
+format_class <- function(self){
+  sprintf(
+    "<%s at %s>\n",
+    class(self)[1],
+    data.table::address(self))
+}
+
+# helper function add sprintf functionality to stop functions.
+stopf = function(fmt, ...){
+  msg = sprintf(fmt, ...)
+  stop(msg, call. = FALSE)
+}
+
