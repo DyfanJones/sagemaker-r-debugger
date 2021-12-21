fe018312b482ac0a7e0f8a6ac24c9752ad458e52
Dyfan Jones <dyfan.jones@theverygroup.com>
2021-12-21 14:53:34 +0000

    bug fix: remove R6 from package name

--- R/debugger_rules_constants.R
+++ R/debugger_rules_constants.R
@@ -2,10 +2,10 @@
 # https://github.com/awslabs/sagemaker-debugger-rulesconfig/blob/master/smdebug_rulesconfig/debugger_rules/_constants.py
 
 RULE_CONFIG_FILE = function(){
-  system.file(file.path("rule_config_jsons", "ruleConfigs.json"), package="R6sagemaker.debugger")
+  system.file(file.path("rule_config_jsons", "ruleConfigs.json"), package="sagemaker.debugger")
 }
 COLLECTION_CONFIG_FILE = function(x){
-  system.file(file.path("rule_config_jsons", "collections.json"), package="R6sagemaker.debugger")
+  system.file(file.path("rule_config_jsons", "collections.json"), package="sagemaker.debugger")
 }
 SUPPORTED_DL_FRAMEWORKS = c("TENSORFLOW", "MXNET", "PYTORCH")
 SUPPORTED_FRAMEWORKS = c("TENSORFLOW", "MXNET", "PYTORCH", "XGBOOST")
